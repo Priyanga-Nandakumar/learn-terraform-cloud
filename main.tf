bab308e8992414b3ffb7dd08d5c5bda69644c3e3
Priyanga-Nandakumar <46834461+Priyanga-Nandakumar@users.noreply.github.com>
2022-06-13 10:52:04 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = var.region
+  access_key = "AKIAZ7ETVKHVCSOTZROL"
+  secret_key = "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"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
